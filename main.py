4616e45645b2d9a706bb8460dbf05811bdf15b28
Morbanaa <theodorerodd@gmail.com>
2026-05-05 15:06:21 -0500

    Here is my solution using a loop, now time to solve this with recursion...

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ def main():
             break
         except ValueError:
             print("You must enter a number")
-def sum_of_number():
-    pass
+    sum_of_number(number)
+
+def sum_of_number(number):
+    total = 0
+    for num in range(number + 1):
+        total+= num
+
+    print(f"Total: {total}")
 
 if __name__ == "__main__":
     main()
