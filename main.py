3cf9beac0892af6f000acdbeea99490dcb8685a2
Morbanaa <theodorerodd@gmail.com>
2026-05-05 16:06:52 -0500

    Sum of numbers adds number to the total then substracts number by one and calls it again using recursion. Then it exits when it is == to 0. Main prints results at the end

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #Sum of Numbers
 
 def main():
+    total = 0
     while True:
         try:
             number = int(input("Enter a number to get the sum from 1 to that number: "))
@@ -12,14 +13,18 @@ def main():
             break
         except ValueError:
             print("You must enter a number")
-    sum_of_number(number)
 
-def sum_of_number(number):
-    total = 0
-    for num in range(number + 1):
-        total+= num
+    # Recives and prints results
+    total = sum_of_number(number,total)
+    print(f"Total sum of 1-{number}: {total}")
 
-    print(f"Total: {total}")
+# Counts sum from 1 to given number
+def sum_of_number(number,total):
+    if number == 0:
+        return total
+    else:
+        return sum_of_number(number - 1,total + number)
 
+# Program Entry Point
 if __name__ == "__main__":
     main()
